a8fd9baf21c2778ba38362386978aa183488b8bd
Aki Evans <akibae4891@gmail.com>
2019-09-19 23:31:22 -0400

    Updated files again

---
+++ .env.py
@@ -0,0 +1,13 @@
+# Environment variable assigments, in DS6_TwitOff. Like bash
+
+ENV = "development"
+DATABASE_URL = "sqlite:///db.sqlite3"
+
+# Add in info from Twitter
+
+TWITTER_CONSUMER_KEY='pMv1gMpvw4Zjrpf9Ikqogf6xA'
+TWITTER_CONSUMER_SECRET='7iGuGWk7jqwJIxoizPMmyrrtZv216m7fuSS3N884y2wNYcaRtf'
+TWITTER_ACCESS_TOKEN='1163838128509116422-sFBjkent5a6r1Z0fKI7DsizsRjPjP8'
+TWITTER_ACCESS_TOKEN_SECRET='uiYJA4olEVfGLdOKT0bWzJ2JoCCxWYl9gB4TJjNH6wjo7'
+
+BASILICA_KEY='08ebe41a-431e-6536-1e33-f74033e46b22'
